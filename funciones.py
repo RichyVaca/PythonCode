1346b4190535b018769c784389294853f5621121
RichyVaca <ric_vaca_9519@outlook.com>
2020-07-28 12:40:21 -0500

    funciones actualizado

--- funciones.py
+++ funciones.py
@@ -1,6 +1,3 @@
-
-
-
 """
 Nombre: funciones.py
 Objetivo: Muestra la operacion de las funciones en python
@@ -27,8 +24,29 @@ def multiplicacion(n1, n2):
 	return n1*n2
 
 def division (n1, n2):
-	return n1/n2
+	if (n2 != 0):
+		return n1/n2
+	else:
+		print ("Error, no se puede dividir entre cero...")
 
+def compara (n1, n2):
+	if n1>n2:
+		print("El mayor es n1: ",n1, " que ",n2)
+	elif n2>n1:
+		print("El mayor es: {} que {}".format(n2,n1))
+	else:
+		print("Los numeros son iguales: {},{}.".format(n1,n2))
+
+#funcion para mostrar operacion  de for
+def cuenta(n1, n2):
+	if (n2>n1):
+		for  i in range(n1, n2+1):
+			print("valor de i: {}".format(i))
+	elif(n1>n2):
+                for  i in range(n1, n2-1, -1):
+                        print("valor de i: {}".format(i))
+	else:
+		print("Los numeros son iguales, no puedo contar: {} , {}".format(n1, n2))
 def main():
 	ciclo = 's'
 	while ciclo == 's' or ciclo == 'S':
@@ -45,6 +63,9 @@ def main():
 		print("La resta de es igual a ", resta(a,b))
 		print("La multiplicacion de es igual a ", multiplicacion(a,b))
 		print("La division de es igual a ", division(a,b))
+		#funcion compara
+		compara(a,b)
+		cuenta(a,b)
 
 		#preguntamos por otra operación
 		ciclo = input("Desea otra operación (S/N)?")
@@ -53,4 +74,3 @@ def main():
 
 if __name__ == "__main__":
 	main()
-
